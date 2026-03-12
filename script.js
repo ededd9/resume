e40f0bb631a2dc7b435dd4c1c23ebb9f2b0b5aed
Edward Hurtado <edhur9@gmail.com>
2026-03-12 17:27:14 -0400

    updated api url with new one after implementing 'IaC'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function updateVisitorCount() {
   const response = await fetch(
-    "https://y8sl0igfie.execute-api.us-east-1.amazonaws.com/count",
+    "https://9igu91n17b.execute-api.us-east-1.amazonaws.com/count",
   );
   const data = await response.json();
   document.getElementById("visitor-count").textContent = data.count;
